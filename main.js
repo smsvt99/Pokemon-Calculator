351ff9905c27ff6586b833404aa2c53959654583
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-09-26 00:32:14 -0400

    separated html, css, js

---
+++ main.js
@@ -0,0 +1,86 @@
+window.onload = function () {
+    fillBaseStats(pokemonStats['bulbasaur']);
+};
+
+document.getElementById('pokemonSelect').addEventListener('change', whichPokemon);
+
+function whichPokemon(event) {
+    let selectedPokemon = event.target.value.toLowerCase();
+    let selectedPokemonStats = pokemonStats[selectedPokemon];
+    fillBaseStats(selectedPokemonStats);
+}
+
+function fillBaseStats(array) {
+    document.getElementById('attackerHP').value = array[0]
+    document.getElementById('attackerAttack').value = array[1]
+    document.getElementById('attackerDefence').value = array[2]
+    document.getElementById('attackerSpeed').value = array[3]
+    document.getElementById('attackerSpecial').value = array[4]
+    document.getElementById('attackerType').value = array[5]
+    document.getElementById('attackerType2').value = array[6]
+}
+
+function getIVs() {
+    let random1 = Math.floor(Math.random() * 16);
+    let random2 = Math.floor(Math.random() * 16);
+    let random3 = Math.floor(Math.random() * 16);
+    let random4 = Math.floor(Math.random() * 16);
+
+    document.getElementById('attackerAttackIV').value = random1;
+    document.getElementById('attackerDefenceIV').value = random2;
+    document.getElementById('attackerSpeedIV').value = random3;
+    document.getElementById('attackerSpecialIV').value = random4;
+
+    document.getElementById('attackerAttackIV2').textContent = random1;
+    document.getElementById('attackerDefenceIV2').textContent = random2;
+    document.getElementById('attackerSpeedIV2').textContent = random3;
+    document.getElementById('attackerSpecialIV2').textContent = random4;
+
+    document.getElementById('attackerAttackIV2Binary').innerHTML =
+        random1.toString(2).padStart(4, '0').substring(0, 3) + '<span id="red">' + random1.toString(2).padStart(4, '0').substring(3) + '</span>';
+
+    document.getElementById('attackerDefenceIV2Binary').innerHTML =
+        random2.toString(2).padStart(4, '0').substring(0, 3) + '<span id="blue">' + random2.toString(2).padStart(4, '0').substring(3) + '</span>';
+
+    document.getElementById('attackerSpeedIV2Binary').innerHTML =
+        random3.toString(2).padStart(4, '0').substring(0, 3) + '<span id="green">' + random3.toString(2).padStart(4, '0').substring(3) + '</span>';
+
+    document.getElementById('attackerSpecialIV2Binary').innerHTML =
+        random4.toString(2).padStart(4, '0').substring(0, 3) + '<span id="orange">' + random4.toString(2).padStart(4, '0').substring(3) + '</span>';
+
+}
+
+function getHPIV() {
+    theNumber =
+        document.getElementById('red').innerText +
+        document.getElementById('blue').innerText +
+        document.getElementById('green').innerText +
+        document.getElementById('orange').innerText
+
+    document.getElementById('binaryResults').innerHTML =
+        '<span id="red2">' +
+        document.getElementById('red').innerText +
+        '</span><span id="blue2">' +
+        document.getElementById('blue').innerText +
+        '</span><span id="green2">' +
+        document.getElementById('green').innerText +
+        '</span><span id="orange2">' +
+        document.getElementById('orange').innerText +
+        '</span>' +
+        ' = ' +
+        parseInt(theNumber, 2)
+
+    colorDigits();
+}
+
+function colorDigits() {
+    document.getElementById('red').style = 'color: red; font-size: 30px;';
+    document.getElementById('blue').style = 'color: blue; font-size: 30px;';
+    document.getElementById('green').style = 'color: green; font-size: 30px;';
+    document.getElementById('orange').style = 'color: orange; font-size: 30px;';
+
+    document.getElementById('red2').style = 'color: red; font-size: 30px;';
+    document.getElementById('blue2').style = 'color: blue; font-size: 30px;';
+    document.getElementById('green2').style = 'color: green; font-size: 30px;';
+    document.getElementById('orange2').style = 'color: orange; font-size: 30px;';
+}
